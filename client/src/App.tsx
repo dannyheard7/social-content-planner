bd3a883eeb03d5160e70291f421732c3f286fac4
Danny Heard <dannyheard7@gmail.com>
2020-02-15 16:41:36 +0000

    Add graphql provider to frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,22 +10,47 @@ import styles from "./App.styles";
 import { makeStyles } from "@material-ui/core";
 import LoginCallback from "./Components/Login/LoginCallback";
 import Dashboard from "./Components/Dashboard/Dashboard";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { setContext } from "apollo-link-context";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
 const useStyles = makeStyles(styles);
 
 const Routes: React.FC = () => {
-  const { isAuthenticated } = useContext(AuthenticationContext);
+  const { isAuthenticated, idToken } = useContext(AuthenticationContext);
 
   if (isAuthenticated) {
+    const httpLink = createHttpLink({
+      uri: process.env.REACT_APP_GRAPHQL_HOST!
+    });
+
+    const authLink = setContext((_, { headers }) => {
+      return {
+        headers: {
+          ...headers,
+          authorization: idToken ? `Bearer ${idToken.__raw}` : ""
+        }
+      };
+    });
+
+    const client = new ApolloClient({
+      link: authLink.concat(httpLink),
+      cache: new InMemoryCache()
+    });
+
     return (
-      <Switch>
-        <Route exact path="/dashboard">
-          <Dashboard />
-        </Route>
-        <Route>
-          <Redirect to="/dashboard" />
-        </Route>
-      </Switch>
+      <ApolloProvider client={client}>
+        <Switch>
+          <Route exact path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route>
+            <Redirect to="/dashboard" />
+          </Route>
+        </Switch>
+      </ApolloProvider>
     );
   } else {
     return (
