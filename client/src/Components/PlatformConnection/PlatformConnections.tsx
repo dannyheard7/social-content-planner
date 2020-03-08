443f625d5db124f4dca80697e532d0c339ff80d2
Danny Heard <dannyheard7@gmail.com>
2020-03-08 18:00:36 +0000

    quick facebook login

--- client/src/Components/PlatformConnection/PlatformConnections.tsx
+++ client/src/Components/PlatformConnection/PlatformConnections.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 import React from "react";
+import FacebookLogin from "react-facebook-login";
 
 const PLATFORM_CONNECTIONS_QUERY = gql`
   query PlatformConnections {
@@ -14,11 +15,19 @@ const PLATFORM_CONNECTIONS_QUERY = gql`
 const PlatformConnections: React.FC = () => {
   const { data, loading } = useQuery(PLATFORM_CONNECTIONS_QUERY);
 
-  if (loading) return <p>Loading</p>
+  if (loading) return <p>Loading</p>;
+
+  // TODO: once we have logged in with facebook, we must get a long-lived access token, then get a long lived page token
 
   return (
     <p>
       {data.platformConnections}
+      <FacebookLogin
+        appId="2002136040088512"
+        autoLoad={true}
+        fields="accounts"
+        callback={console.log}
+      />
     </p>
   );
 };
